9f8f3a754e7132447b39d58720877bd0ee257493
riwajghimire <rewajghimire@gmail.com>
2025-03-20 13:52:32 +0545

    Projects

---
+++ scrapper.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+URL = "https://books.toscrape.com/"
+
+
+def scrape_books(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Failed To Fetch Data...")
+        return
+    response.encoding = response.apparent_encoding # used to remove special characters of euros
+    soup = BeautifulSoup(response.text,"html.parser")
+    books = soup.find_all("article", class_="product_pod")
+    print(books)
+    scraped_books = []
+    for book in books:
+        title = book.h3.a['title'] # title in [] beacuse its not a tag but an attribute
+        price_text = book.find("p", class_="price_color").text
+        currency = price_text[0]
+        price = price_text[1:]
+        dict = {
+            "title":title,
+            "currency":currency,
+            "price":price
+
+        }
+        scraped_books.append(dict)
+    with open("data.json", "w",encoding='utf-8') as f:
+            json.dump(scraped_books,f,indent=4,ensure_ascii=False)
+
+
+scrape_books(URL)
+# install git
+# create repository in github
+# go to git bash
+# git config --global user.name "riwaj ghimire"
+# git config --global user.email "riwajghimire@gmail.com"
+# git init
+# git status => check status of files
+# git diff => changes in files
+# git add . 
+# git commit -m "your message"
+# copy paste git code from github
+
